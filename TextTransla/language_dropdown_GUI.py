33a374fe73094502b9b8583644212c51bef78f27
Faraz <farazakb@buffalo.edu>
2024-11-09 22:30:10 -0500

    added title and made it centered and blue

--- TextTransla/language_dropdown_GUI.py
+++ TextTransla/language_dropdown_GUI.py
@@ -10,9 +10,11 @@ def main():
     root.title("Instant Language Translator")
     root.state('zoomed')
     root.geometry("800x600")
+    root.rowconfigure(1, weight = 1)
+    root.columnconfigure(1, weight = 1)
 
-    proj_label = tk.Label(root, text = "YSL UMAIR", font = ("Helvetica", 35), bg = "white", fg = "black")
-    proj_label.grid(row=4, column=15, columnspan=3, pady=8, padx=12, sticky='nsew')
+    proj_label = tk.Label(root, text = "Instant Language Translator", font = ("Helvetica", 35), bg = "white", fg = "blue")
+    proj_label.grid(row=0, column = 0, columnspan = 5, padx = 10, pady = 10, sticky = "nsew")
 
 
 
