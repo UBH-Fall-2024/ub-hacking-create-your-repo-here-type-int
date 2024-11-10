f9bf41a05d641059e6a13ff997711c13fd367e7f
umairaali58 <umairaali58@gmail.com>
2024-11-09 21:55:04 -0500

    testing for translate

--- TextTransla/simple_text_trans.py
+++ TextTransla/simple_text_trans.py
@@ -1,6 +1,11 @@
 from deep_translator import GoogleTranslator
 
 def translatetext(text):
-    translated = GoogleTranslator(source='auto', target=).translate(text)  # output -> Weiter so, du bist großartig
+    translated = GoogleTranslator(source='auto', target="en").translate(text)  # output -> Weiter so, du bist großartig
+    print(translated)
 
-    
+def test1():
+    translatetext("أنا أحب علوم الكمبيوتر")
+
+if __name__ == '__main__':
+    test1()
