e26a02fd099d2881c5bb773793d7ac2df2c0651c
Faraz <farazakb@buffalo.edu>
2024-11-09 21:58:36 -0500

    yuh

--- TextTransla/language_dropdown_GUI.py
+++ TextTransla/language_dropdown_GUI.py
@@ -1,3 +1,15 @@
+import os
+import tkinter as tk
+from tkinter import ttk
+
+def main():
+    print("yuh")
+
+def click_buttion():
+    return
+
+def language_tab():
+    return
 
 def gui_language_dropdown():
-    
+    return
