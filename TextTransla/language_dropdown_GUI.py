dd132247374820b705c3a3900b82b957365dfa81
Faraz <farazakb@buffalo.edu>
2024-11-09 22:16:30 -0500

    added title

--- TextTransla/language_dropdown_GUI.py
+++ TextTransla/language_dropdown_GUI.py
@@ -3,13 +3,31 @@ import tkinter as tk
 from tkinter import ttk
 
 def main():
-    print("yuh")
+    root = tk.Tk()
+    root.title("Instant Language Translator")
+    root.state('zoomed')
+    root.geometry("800x600")
 
-def click_buttion():
+    proj_label = tk.Label(root, text = "YSL UMAIR", font = ("Helvetica", 35), bg = "white", fg = "black")
+    proj_label.grid(row=4, column=15, columnspan=3, pady=8, padx=12, sticky='nsew')
+
+
+
+
+
+
+    root.mainloop()
+
+
+#button that is clicked which sends user to web app
+def click_button():
     return
 
+# dictionary for language translated and targets
 def language_tab():
     return
 
 def gui_language_dropdown():
     return
+
+main()
