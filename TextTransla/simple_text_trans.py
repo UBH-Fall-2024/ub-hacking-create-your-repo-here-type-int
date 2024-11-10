39fd3f8a20a23bc634fc829cfc2b6858dd82722a
tahahayali <tahahayali05@gmail.com>
2024-11-09 20:32:17 -0500

    Adds trans

--- TextTransla/simple_text_trans.py
+++ TextTransla/simple_text_trans.py
@@ -1,4 +1,9 @@
 from deep_translator import GoogleTranslator
 
+def ChooseLanguage():
+    language = input("")
+
 def translatetext(text):
+    translated = GoogleTranslator(source='auto', target='de').translate(text)  # output -> Weiter so, du bist großartig
+
     
